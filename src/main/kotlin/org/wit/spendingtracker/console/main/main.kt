a2c2a83341b090dcb0f8b48e1547c2e46cb696f1
StephenMalone99 <stephenmalone99@hotmail.com>
2021-10-02 15:36:49 +0100

    Pushing all initial files for testing

---
+++ src/main/kotlin/org/wit/spendingtracker/console/main/main.kt
@@ -0,0 +1,10 @@
+package org.wit.spendingtracker.console.main
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+
+fun main(args: Array<String>) {
+    logger.info { "Launching my Spending Tracker App" }
+    println("This is Version 1.0")
+}
