f9f3caa421f6efe71d4447147801c08a3843c90f
StephenMalone99 <stephenmalone99@hotmail.com>
2021-10-02 17:48:40 +0100

    Beginning to add CRUD functionaltiy for purchases (in line with labs)

---
+++ src/main/kotlin/org/wit/spendingtracker/console/models/PurchasesModel.kt
@@ -0,0 +1,5 @@
+package org.wit.spendingtracker.console.models
+
+data class PurchasesModel(var id: Long = 0,
+                          var description: String = "",
+                          var amount : Int = 0)
